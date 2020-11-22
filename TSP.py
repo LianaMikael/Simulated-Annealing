2ad008fd0e95252825e129b03b9a4af52ebe515b
Liana Mikaelyan <mvl1ana98@gmail.com>
2020-11-22 19:48:41 +0300

    updated algorithm

--- TSP.py
+++ TSP.py
@@ -13,7 +13,7 @@ class Map:
         self.x_nodes = np.random.rand(self.nodes_num,1) * self.width
         self.y_nodes = np.random.rand(self.nodes_num,1) * self.height
 
-    def visualize(self, show_path=True):
+    def visualize(self, cost=None, temp=None, show_path=True):
         plt.title('Map')
         plt.xlim(0,self.width)
         plt.ylim(0,self.height)
@@ -23,6 +23,11 @@ class Map:
                 plt.plot(self.x_nodes[i:i+2], self.y_nodes[i:i+2], 'bo-')
             # return back to original node
             plt.plot([self.x_nodes[-1],self.x_nodes[0]],[self.y_nodes[-1:],self.y_nodes[0]], 'bo-')
+
+            if cost and temp:
+            # visualize the total cost 
+                plt.text(1, 4.8, 'cost: {:.2f}'.format(cost), color='r')
+                plt.text(1, 4.5, 'temp: {:.2f}'.format(temp), color='r')
 
         plt.plot(self.x_nodes[0], self.y_nodes[0], 'o', color='r')
         plt.plot(self.x_nodes[1:], self.y_nodes[1:], '.')
@@ -38,9 +43,9 @@ class SimulatedAnnealing:
 
     def anneal(self):
         curr_permutation = np.hstack([self.map.x_nodes, self.map.y_nodes])
-
-        for i in range(self.max_iter):
-            print(curr_permutation)
+        costs = []
+        for _ in range(self.max_iter):
+            
             curr_cost = self.cost(curr_permutation)
             
             new_permutation = self.create_neighbour_path(curr_permutation)
@@ -50,22 +55,29 @@ class SimulatedAnnealing:
             if new_cost < curr_cost:
                 curr_permutation = new_permutation
                 curr_cost = new_cost
-                
+                self.map.x_nodes = new_permutation[:,0]
+                self.map.y_nodes = new_permutation[:,1]
+
             elif np.random.random() < self.acceptance_prob(curr_cost, new_cost):
                 curr_permutation = new_permutation
                 curr_cost = new_cost
+                self.map.x_nodes = new_permutation[:,0]
+                self.map.y_nodes = new_permutation[:,1]
 
+            self.map.visualize(curr_cost, self.temp)
+
+            costs.append(curr_cost)
             self.temp *= self.rate 
 
     def acceptance_prob(self, curr_cost, new_cost):
         # calculates the acceptance probability 
-        return np.exp(np.abs(curr_cost - new_cost) / self.temp)
+        return np.exp(-np.abs(curr_cost - new_cost) / self.temp)
 
     @staticmethod
     def create_neighbour_path(curr_permutation, visualize=False):
-        # swaps two randomly chosen nodes 
-        idx_1 = np.random.randint(len(curr_permutation))
-        idx_2 = np.random.randint(len(curr_permutation))
+        # swaps two randomly chosen nodes (apart form the origin)
+        idx_1 = np.random.randint(1,len(curr_permutation))
+        idx_2 = np.random.randint(1,len(curr_permutation))
 
         while (idx_1 == idx_2): 
             idx_2 = np.random.randint(len(curr_permutation))
--- solver.py
+++ solver.py
@@ -6,15 +6,21 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer('width', 10, 'width of the map')
 flags.DEFINE_integer('height', 5, 'height of the map')
 flags.DEFINE_integer('nodes_num', 20, 'number of nodes on the map')
+flags.DEFINE_float('temp', 1000, 'initial temperature')
+flags.DEFINE_float('temp_rate', 0.95, 'rate at which to decrease the temperature')
+flags.DEFINE_integer('max_iter', 5000, 'maximum number of iterations to anneal')
 
 def main(_):
     width = FLAGS.width
     height = FLAGS.height
     nodes_num = FLAGS.nodes_num
+    temp = FLAGS.temp
+    temp_rate = FLAGS.temp_rate
+    max_iter = FLAGS.max_iter 
 
     random_map = Map(width, height, nodes_num)
-    s = SimulatedAnnealing(random_map, 0.8, 0.9, 1000).anneal()
     random_map.visualize()
+    SimulatedAnnealing(random_map, temp, temp_rate, max_iter).anneal()
 
 if __name__ == '__main__':
     app.run(main)
